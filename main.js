536c0504530cb3af75ae3d5c60890cc0e83e9d6f
Joshvu93 <joshvu93@gmail.com>
2019-10-19 13:37:48 -0700

    Upload working

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 $(document).ready(function (data) {
     $.getJSON("data.json", function (data) {
-        $each(data, function (index, value) {
+        $.each(data, function (index, value) {
             $("table").append("<tr><td>"
                 + value.name + "</td><td>"
                 + value.description + "</td><td><a href=https://www.google.com/maps?q="
-                + value.location + ">Open in Google Maps</a></td></tr>");
+                + value.location + " target='_blank'><button id='google-button'>Google Maps</button></a></td></tr>");
         });
     });
 });
